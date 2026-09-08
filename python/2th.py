b7f31f5d753d408c987f730fa285c6d189632c57
suan choi <suan3214@gmail.com>
2026-09-08 15:59:35 +0900

    하위 폴더 및 파일 추가

---
+++ python/2th.py
@@ -0,0 +1,19 @@
+print("1 - hello world")
+print(10 + 10)
+print(30 - 10)
+print(30* 30)
+print(30/10)
+print("30" "*" "30" "=" , 30*30)
+print("123" "*" "456" "=" ,123*456)
+
+a = str(input("첫번째 값 :"))
+b = str(input("두번째 값 :"))
+##result = a*b
+##result1= a/b
+##result2=a-b
+##result3=a+b
+##print(a, "*", b, "=", result)
+##print(a, "/", b, "=", result1)
+##print(a, "-", b, "=", result2)
+##print(a, "+", b, "=", result3)
+print(a, "and", b)
